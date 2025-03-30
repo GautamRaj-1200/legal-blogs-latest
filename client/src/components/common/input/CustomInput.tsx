191d229e69eda9a83abcece8f5d7a2b26364e4cd
GautamRaj-1200 <gautamrajv2@gmail.com>
2025-03-30 14:16:41 +0530

    feat(client): add input custom component

--- client/src/components/common/input/CustomInput.tsx
+++ client/src/components/common/input/CustomInput.tsx
@@ -6,41 +6,49 @@ interface CustomInputProps {
   label: string;
   value: string;
   error: string;
+  hint: string;
   className?: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
   onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
+
 const CustomInput = ({
   type,
   name,
   label,
   value,
   error,
+  hint,
   className = '',
-  onChange = () => undefined,
-  onFocus = () => undefined,
-  onBlur = () => undefined,
+  onChange,
+  onFocus,
+  onBlur,
 }: CustomInputProps) => {
   return (
-    <>
-      <div className={styles.group}>
-        <input
-          type={type}
-          id={label}
-          name={name}
-          className={`${styles['group-input']} ${className}`}
-          value={value}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          onChange={onChange}
-        />
-        <label htmlFor={label}>{label}</label>
-        <div className={`${styles['error-container']} ${error ? styles['has-error'] : ''}`}>
-          <p className={styles['input-error']}>{error}</p>
-        </div>
+    <div className={styles.group}>
+      <label htmlFor={name} className={styles['input-label']}>
+        {label}
+      </label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className={`${styles['group-input']} ${className}`}
+        value={value}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onChange={onChange}
+      />
+      {/* Hint Message (Only show when no error is present) */}
+      <div className={`${styles['hint-container']} ${hint && !error ? styles['show-hint'] : ''}`}>
+        <p className={styles['input-hint']}>{hint}</p>
       </div>
-    </>
+      {/* Error Message */}
+      <div className={`${styles['error-container']} ${error ? styles['has-error'] : ''}`}>
+        <p className={styles['input-error']}>{error}</p>
+      </div>
+    </div>
   );
 };
 
