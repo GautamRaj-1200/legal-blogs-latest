50c4fc4338a9769efadac99804d65b4a78e82489
GautamRaj-1200 <gautamrajv2@gmail.com>
2025-04-17 10:44:25 +0530

    refactor(app): modify rate limiting

--- app/src/utils/rateLimitter.ts
+++ app/src/utils/rateLimitter.ts
@@ -4,9 +4,14 @@ import rateLimit from 'express-rate-limit';
 export const otpRequestLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 3, // limit each IP/email to 3 OTP requests per 15 mins
-  message: 'Too many OTP requests. Please try again after 15 minutes.',
   standardHeaders: true,
   legacyHeaders: false,
+  handler: (req, res, next, options) => {
+    res.status(options.statusCode).json({
+      message: options.message,
+    });
+  },
+  message: 'Too many OTP requests. Please try again after 15 minutes.',
 });
 
 export const loginLimiter = rateLimit({
@@ -14,6 +19,11 @@ export const loginLimiter = rateLimit({
   max: 5, // Max 5 login attempts per 15 mins per IP
   standardHeaders: true,
   legacyHeaders: false,
+  handler: (req, res, next, options) => {
+    res.status(options.statusCode).json({
+      message: options.message,
+    });
+  },
   message: 'Too many login attempts. Please try again after 15 minutes.',
 });
 
@@ -22,5 +32,10 @@ export const signupLimiter = rateLimit({
   max: 3, // Only 3 signups allowed per IP per hour
   standardHeaders: true,
   legacyHeaders: false,
+  handler: (req, res, next, options) => {
+    res.status(options.statusCode).json({
+      message: options.message,
+    });
+  },
   message: 'Too many accounts created from this IP, please try again later.',
 });
