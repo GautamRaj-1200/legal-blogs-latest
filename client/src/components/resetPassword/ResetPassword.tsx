8e5800b2daa0bc28bcaf3d8ec87da4fa826854b5
GautamRaj-1200 <gautamrajv2@gmail.com>
2025-05-16 23:22:32 +0530

    refactor(client): refactor reset password - might be temp fix

--- client/src/components/resetPassword/ResetPassword.tsx
+++ client/src/components/resetPassword/ResetPassword.tsx
@@ -1,13 +1,21 @@
 import Button from '../common/button/Button';
 import Otp from '../common/otp/Otp';
 import styles from './ResetPassword.module.css';
+import { useState } from 'react';
 
 const ResetPassword = () => {
+  const [otp, setOtp] = useState('');
+
   return (
     <>
       <section className={styles['reset__password-otp']}>
         <h2>Reset Password</h2>
-        <Otp />
+        <Otp
+          otp={otp.split('')}
+          setOtp={(value: string[]) => {
+            setOtp(value.join(''));
+          }}
+        />
         <div className="btn__container">
           <Button>Reset</Button>
         </div>
